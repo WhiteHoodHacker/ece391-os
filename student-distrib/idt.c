3f71c52a4ff737b7cb683c00df4ece5592dc9469
Emma Hartman <emmaih2@illinois.edu>
2023-03-19 18:14:50 -0500

    updated idt test cases

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f5da9b00907299c3b171a6065d7c59f18d53a49b615e403229a2df8fe2fac52
-size 832
+oid sha256:21cfed945163172454a70c0190d57845c97a4d45ed23dc3718c4b5ebff6756df
+size 902
