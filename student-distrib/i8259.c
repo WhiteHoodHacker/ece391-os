572c9ebf0d6127bfb48348011dba80bec03bb51b
Philip Hu <pfhu2@illinois.edu>
2023-03-19 19:36:15 -0500

    Swtiched OUTB order

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb96c1780e060deb0d2e55a3b0e2604a29fc9961097e6327ee9fe9a5114be3c4
-size 2176
+oid sha256:46841b6e4fae2ff20707d2a2c08e88140133b213442fa190640b3e4d4f039e6a
+size 2174
