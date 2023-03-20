de6bbf42c15d0508cc702fca82422f7b070b3368
Minh Duong <git@whitehoodhacker.net>
2023-03-20 15:27:46 -0500

    Add debug exception

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:583c508dc9d9eca7b9e889a5922a881436002a86aadfadcc5620a32aff5a2689
-size 2118
+oid sha256:a7f07ce9312da26942b089b54e3be9c38429d1b07cceef779bad1295e0e48fdc
+size 2178
--- student-distrib/isr.c
+++ student-distrib/isr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1befbd7d85260b5fef4dee36e87fc1902fcbd3a8aee849f6a161cdb0a4518c3b
-size 2530
+oid sha256:ebf7e921c8f2f5665637d6790c3bb9750b5f84937a5ffc4be04e724e91b8dbf2
+size 2626
