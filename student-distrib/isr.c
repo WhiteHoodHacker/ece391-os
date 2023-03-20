b8a1dcb58367c7327c1713fcc91bc49812b6b247
Minh Duong <git@whitehoodhacker.net>
2023-03-20 15:24:02 -0500

    Added comments about error code, better exception message format

--- student-distrib/isr.c
+++ student-distrib/isr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5473f8ec5abac00805bcb5b398f49d8238c0de8e442f31f1d4053887c3246d4
-size 2236
+oid sha256:1befbd7d85260b5fef4dee36e87fc1902fcbd3a8aee849f6a161cdb0a4518c3b
+size 2530
