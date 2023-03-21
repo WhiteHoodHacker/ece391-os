5d0314ec484a21e39814e5477eda5d175c05a227
Hartman <emmaih2@illinois.edu>
2023-03-21 14:44:36 -0500

    finished off test cases

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4ae3b1c63fe5f3e28007ccb7957e6642a22da15f80df2926ec3c98994373295
-size 10245
+oid sha256:65d17dedfa4bccc6e40712e16c69e03359ce70e7f196bbaa37be907013c3cabf
+size 9675
