fdb174fac98772c654c2e31fb8d52ac826763f95
Hu <pfhu2@illinois.edu>
2023-03-26 18:54:34 -0500

    Figured out how to print file names I guess'

--- student-distrib/filesys.c
+++ student-distrib/filesys.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42a42c457baa3bd9798c2b30be87cc466bff61e396de730168eb1c386cb2993f
-size 4810
+oid sha256:0286f09056a99d52874d69e64ed69bb4376a409758dde8920034f7f9b5753123
+size 5135
