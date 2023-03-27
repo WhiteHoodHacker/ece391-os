e69145db9cd32aed02c9a882796856b10bd440f6
ankitatmidnight <82774113+ankitatmidnight@users.noreply.github.com>
2023-03-27 09:49:47 -0500

    fix edgecase in newline

--- student-distrib/lib.c
+++ student-distrib/lib.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:858de930a9fce8f4daecd5bd380f82ad88f02970ef3ab2e4e6517e6b5f8f74cd
-size 18056
+oid sha256:f1307eb9bdcd0542426eb8fe8367eb5e0bb619a723c9a7fa03cb7c9fa746ecbb
+size 18300
