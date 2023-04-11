1af189ce27aaa4004e5201608460e5ccf93ee949
ankitatmidnight <82774113+ankitatmidnight@users.noreply.github.com>
2023-04-11 14:46:47 -0500

    end of halt causing page fault

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdb846f2686c679f4532960420af605e4a1e803be622847043daa05e078af921
-size 8771
+oid sha256:6912bf60db4090cea45cff58125ea701cee68fe48aa0959d811cf74f7288f393
+size 8921
