b4c4d5850fcbe23c2189bdfe25518b505f779489
Hartman <emmaih2@illinois.edu>
2023-04-11 01:05:48 -0500

    finished draft of halt

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f50facce9f28cffd40ba7265bdabfd6c37009bea1943d9399ac070017faaf80
-size 5962
+oid sha256:ce7fe9aab0b5ce2179d78eb10d073ec2ed9930860a482de0542a79afabd0d90d
+size 6014
