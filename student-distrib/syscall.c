eb1a677904349cd9a45866ae43144eaa5aa3e5ab
Hu <pfhu2@illinois.edu>
2023-04-13 19:24:16 -0500

    Halt and shell quit working

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee5d73b2a9ed2b8ebbf173e867a5a75d7bc8f86e54c83a4fad3662f7f01e851e
-size 10716
+oid sha256:6f11c2cc0c432c38b3bcdc0f61006a0e94e6eecb9d5c8da773ba27e19f9e83cb
+size 10725
