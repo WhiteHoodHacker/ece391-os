d863e278cfb78db96a9593ebff2b164627d08b0d
Hartman <emmaih2@illinois.edu>
2023-04-14 19:48:20 -0500

    removed print statement

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05e7051021ecbcaa915f5954748fc0f9dd3cb9456638069b8ca4f90b4178037d
-size 10788
+oid sha256:1dda1c9e5cc074ae01847b3e52d7e4ffc75de7b2152b647c3c744d784143f791
+size 10767
