3c7b53c9914da3d8109326d05293018544b7652d
Minh Duong <minhd2@illinois.edu>
2023-04-16 23:53:04 -0500

    Reduce process memory size

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b368a4422aa304934e8fa918f72d5547e162f30ee2e2d03d8589681d442c4e7
+oid sha256:bf76a007b7bceb0ef45481c1491ecfff84271b2d0b70623cb0d06c9e2d1e1877
 size 5617
