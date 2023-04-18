8c9454a42bd85804e868d6032071474b96862245
Minh Duong <minhd2@illinois.edu>
2023-04-18 01:24:07 -0500

    Fix newline setting text color to black

--- student-distrib/lib.c
+++ student-distrib/lib.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd78f6207a30649d27e5e14de7401b7af98af96c15ab88c66f026e9d0b33958
-size 18998
+oid sha256:7b654c4153020f79c9b946dc8f8a2372af83618718c1f2e234e1cd0d35e0342e
+size 19274
