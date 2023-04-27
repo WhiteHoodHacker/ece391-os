b3391573d60627675044ac0a3b92cc3dc1cb84ab
Minh Duong <minhd2@illinois.edu>
2023-04-27 16:27:06 -0500

    Minor format changes

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fe1d9bf4c0815c99d9c61e13025dd98c54e7ed62aba6c4749852620daeb3434
-size 3781
+oid sha256:b549e6fed89688472fc89512e4fd23abbbec1df9dfbfaa7bfc86d3d3ed7ef41c
+size 3778
